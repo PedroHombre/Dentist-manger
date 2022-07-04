4807a6e0637fcadbe16673647c8d59d19d27681e
PedroHombre <radekgornackiv2@gmail.com>
2022-07-04 12:28:33 +0200

    Database plan and register form basic html

---
+++ panel/register.php
@@ -0,0 +1,29 @@
+<!-- TODO:
+- field animations
+- styles
+- realtime password strength calculator
+- js validations
+- php validations
+- loggedin redirect -->
+
+<form action="POST">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name">
+    <br />
+
+    <label for="surname">Surname</label>
+    <input type="text" name="surname">
+    <br />
+
+    <label for="mail">E-mail</label>
+    <input type="email" name="Email" id="email">
+    <br />
+
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password">
+    <br />
+
+    <label for="repeat password">Repeat password</label>
+    <input type="password" name="repeatPassword" id="repeatPassword">
+    <br />
+</form>
