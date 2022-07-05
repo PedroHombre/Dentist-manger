8aaca26c3597416eb1c3103702748bda4849b579
PedroHombre <radekgornackiv2@gmail.com>
2022-07-05 08:32:08 +0200

    to be fixed

--- panel/js/register-validation.js
+++ panel/js/register-validation.js
@@ -7,9 +7,13 @@ let email = document.getElementById('email');
 let password = document.getElementById('password');
 let passwordConfirm = document.getElementById('repeatPassword');
 
+formSubmit.addEventListener('click', Validate);
+
 let nameValidated = false;
 let surnameValidated = false;
 let emailValidated = false;
+let passwordValidated = false;
+let passwordConfirmValidated = false;
 
 nameF.addEventListener('input', () => {
 	if (nameF != '') nameValidated = true;
@@ -22,3 +26,23 @@ surname.addEventListener('input', () => {
 email.addEventListener('input', () => {
 	if (email != '') emailValidated = true;
 });
+
+password.addEventListener('input', () => {
+	if (password != '') passwordValidated = true;
+});
+
+passwordConfirm.addEventListener('input', () => {
+	if (passwordConfirm == password) passwordConfirmValidated = true;
+});
+
+function Validate() {
+	if (
+		nameValidated === true &&
+		surnameValidated === true &&
+		emailValidated === true &&
+		passwordValidated === true &&
+		passwordConfirmValidated === true
+	) {
+		registerForm.Submit();
+	}
+}
